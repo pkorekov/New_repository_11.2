0f3a11463118ae5fcd37658e9ed852955e461fe8
Pavel Korekov <pkorekov@gmail.com>
2023-05-29 21:56:22 +0500

    add str_func.py file add upper_str func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def upper_str(value):
+    '''Получаем строку заглавными буквами'''
     return value.upper()
+
+def title_str(value):
+    '''Делает заглавными первые буквы каждого слова в строке'''
+    return value.title()
